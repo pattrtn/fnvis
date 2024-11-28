b96341f91c2991c2b893474ca371bc9e44e47e71
Pattraporn T. <146692261+pattrtn@users.noreply.github.com>
2024-11-28 20:54:57 +0700

    Update fn.py

--- fn.py
+++ fn.py
@@ -3,21 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load data into a DataFrame
-data = {
-    'CNTSCHID': [76400001, 76400001, 76400001, 76400001, 76400001],
-    'CNTSTUID': [76400396, 76400632, 76400865, 76400936, 76401306],
-    'ST001D01T': [11, 10, 10, 10, 10],
-    'ST003D02T': [8, 1, 11, 2, 7],
-    'ST003D03T': [2006, 2007, 2006, 2007, 2006],
-    'ST004D01T': [2, 1, 1, 2, 1],
-    'ST250D06JA': [7640001, 7640002, 7640001, 7640001, 7640001],
-    'ST250D07JA': [7640002, 7640002, 7640002, 7640002, 7640002],
-    'PV1MATH': [376.167, 374.905, 439.850, 430.583, 279.361],
-    'PV1READ': [255.171, 453.844, 423.108, 444.154, 320.895],
-    'PV1SCIE': [391.449, 385.540, 481.047, 413.090, 315.831]
-}
-df = pd.DataFrame(data)
+# Load data for mapping
+geo_data_path = './pisa-pisa.csv'
+df = pd.read_csv(geo_data_path, encoding='utf-8')
 
 # Streamlit app layout
 st.title('Educational Data Analysis Dashboard')
